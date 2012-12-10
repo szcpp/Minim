5c6ce267ade35a6f2d52bf0010994bc297a56124
annna <@pl>
2012-12-10 23:28:26 +0100

    comments to class Plot()

--- inc/Plot.hpp
+++ inc/Plot.hpp
@@ -2,21 +2,19 @@
 #define _PLOT_HPP
 
 #include "plstream.h"
-#include <unistd.h>
-
-using namespace std;
+#include <sstream>
 
 class Plot
 {
 public:
 	Plot(float min, float max, int bin);
 	~Plot(){delete pls;}
-	void DrawHist(int liczba, int gen, unsigned short*  dane);
+	void DrawHist(int liczba, int gen, unsigned short* dane); // przyjmuje liczbę punktów do umieszczenia i tablicę wartości
 private:
 	plstream* pls;
-	PLFLT minR;
-	PLFLT maxR;
-	PLINT binN;
+	PLFLT minR; // minimum osi poziomej
+	PLFLT maxR; // maksimum osi poziomej
+	PLINT binN; // liczba binów
 };
 
 #endif
--- src/Plot.cpp
+++ src/Plot.cpp
@@ -1,24 +1,22 @@
 #include "Plot.hpp"
-#include <sstream>
 
 Plot::Plot(float min, float max, int bin): minR(min), maxR(max), binN(bin)
 {
-	// plplot initialization
 	pls = new plstream();
-	pls->sdev ("xwin");
+	pls->sdev ("xwin"); //może "tk" ? 
 	pls->init();
-	pls->spause(0);
-	//delete pls;
+	pls->spause(0); // blokowanie czekania na np enter
 }
 
 void Plot::DrawHist(int liczba, int gen, unsigned short* dane)
 {
 	PLFLT* data = new PLFLT[liczba];
-	for(int i = 0; i<liczba; i++) data[i]=(double)dane[i];
-	pls->col0( 3 );
+	for(int i = 0; i<liczba; i++) data[i]=(double)dane[i]; // można zmienić to przepisywanie (=przyspieszyć) jeśli uda Ci się zmienić tablicę short na tablicę doubli (PLFLT to double)
+	pls->col0( 3 ); // kolor wykresu
 	pls->hist( liczba, data, minR, maxR, binN, PL_HIST_IGNORE_OUTLIERS );
-	pls->col0( 4 );
-	stringstream gg;
+	pls->col0( 4 ); //kolor napisów
+	std::stringstream gg;
 	gg<<" Generacja " << gen+1;
 	pls->lab( "r", "liczba osobnikow", gg.str().c_str() );
 }
+
